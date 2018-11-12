9643a725bc17e1980f60ef2f6444f46490bae991
Miguel Rincon <miguel.rincon86@gmail.com>
2018-11-12 08:51:15 +0800

    Answer 3, pages with more than 1% of errors

--- logs_analysis.py
+++ logs_analysis.py
@@ -9,6 +9,7 @@ def main():
   conn = psycopg2.connect(dbname='news')
   most_popular_articles(conn)
   most_popular_authors(conn)
+  more_1_per_centent_errors(conn)
   conn.close()
   print('Analysis is finished.')
 
@@ -42,6 +43,28 @@ def most_popular_authors(conn):
   cur.close()
 
   print_results('Answer 2 - Top authors by views are:', result)
+  return result
+
+def more_1_per_centent_errors(conn):
+  cur = conn.cursor()
+  cur.execute('''
+    SELECT views.day, round((errors.count * 100.0 / views.count), 2) as percent_errors
+    FROM
+      (SELECT date_trunc('day', time) as day, count(*) as count
+      FROM log
+      GROUP BY day) as views
+    JOIN
+      (SELECT date_trunc('day', time) as day, count(*) as count
+      FROM log
+      WHERE status NOT LiKE '2__ %'
+      GROUP BY day) as errors
+    ON views.day = errors.day
+    WHERE round((errors.count * 100.0 / views.count), 2) > 1;
+  ''')
+  result = cur.fetchall()
+  cur.close()
+
+  print_results('Answer 3 - Not done:', result, '% errors')
   return result
 
 def print_results(header, results, units='views'):
