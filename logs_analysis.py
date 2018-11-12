576ed14c53981f133d86cd314b14981fac097333
Miguel Rincon <miguel.rincon86@gmail.com>
2018-11-12 08:15:36 +0800

    Answer 2, most popular authors

--- logs_analysis.py
+++ logs_analysis.py
@@ -8,6 +8,7 @@ def main():
   print('Analysis will start...')
   conn = psycopg2.connect(dbname='news')
   most_popular_articles(conn)
+  most_popular_authors(conn)
   conn.close()
   print('Analysis is finished.')
 
@@ -19,12 +20,28 @@ def most_popular_articles(conn):
     WHERE log.path LIKE '/article/' || articles.slug
     GROUP BY articles.title
     ORDER BY views DESC
-    LIMIT 3;
+    LIMIT 10;
     ''')
   result = cur.fetchall()
   cur.close()
 
   print_results('Answer 1 - Top 3 articles are:', result)
+  return result
+
+def most_popular_authors(conn):
+  cur = conn.cursor()
+  cur.execute('''
+    SELECT authors.name, count(*) as views
+    FROM authors, articles, log
+    WHERE authors.id = articles.author
+      AND log.path LIKE '/article/' || articles.slug
+    GROUP BY authors.name
+    ORDER BY views DESC
+    ''')
+  result = cur.fetchall()
+  cur.close()
+
+  print_results('Answer 2 - Top authors by views are:', result)
   return result
 
 def print_results(header, results, units='views'):
